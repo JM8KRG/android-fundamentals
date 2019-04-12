29dd4156e9c454e0a406a96b305b5f2d15762942
macaron <keita.nishi@moi.st>
2019-04-12 18:38:19 +0900

    Add Async Task (device rotation support)

--- SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -7,8 +7,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private TextView mTextView;
+
+    private static final String TEXT_STATE = "currentText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = findViewById(R.id.textView1);
+
+        if (savedInstanceState != null) {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
     }
 
     public void startTask(View view) {
         mTextView.setText(R.string.napping);
         new SimpleAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
     }
 }
